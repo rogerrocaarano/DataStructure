879aafad60c6318106d41e2fdbfba68c68a2f2af
Róger Roca Arano <rogerroca@reinseg.com>
2022-10-17 17:08:09 -0400

    list: Renamed to listSimRAM. listSimRAM: added procedure to show RAM. listSIMRAM: added exception for empty list listSimRAM.cpp line 114.

---
+++ cmake-build-debug/CMakeFiles/RAMSimulator.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rogerroca/CLionProjects/RAMSimulator/Polynomial.cpp" "CMakeFiles/RAMSimulator.dir/Polynomial.cpp.obj" "gcc" "CMakeFiles/RAMSimulator.dir/Polynomial.cpp.obj.d"
+  "C:/Users/rogerroca/CLionProjects/RAMSimulator/listSimRAM.cpp" "CMakeFiles/RAMSimulator.dir/listSimRAM.cpp.obj" "gcc" "CMakeFiles/RAMSimulator.dir/listSimRAM.cpp.obj.d"
+  "C:/Users/rogerroca/CLionProjects/RAMSimulator/main.cpp" "CMakeFiles/RAMSimulator.dir/main.cpp.obj" "gcc" "CMakeFiles/RAMSimulator.dir/main.cpp.obj.d"
+  "C:/Users/rogerroca/CLionProjects/RAMSimulator/simRAM.cpp" "CMakeFiles/RAMSimulator.dir/simRAM.cpp.obj" "gcc" "CMakeFiles/RAMSimulator.dir/simRAM.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RAMSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RAMSimulator.dir/Polynomial.cpp.obj"
+  "CMakeFiles/RAMSimulator.dir/Polynomial.cpp.obj.d"
+  "CMakeFiles/RAMSimulator.dir/listSimRAM.cpp.obj"
+  "CMakeFiles/RAMSimulator.dir/listSimRAM.cpp.obj.d"
+  "CMakeFiles/RAMSimulator.dir/main.cpp.obj"
+  "CMakeFiles/RAMSimulator.dir/main.cpp.obj.d"
+  "CMakeFiles/RAMSimulator.dir/simRAM.cpp.obj"
+  "CMakeFiles/RAMSimulator.dir/simRAM.cpp.obj.d"
+  "RAMSimulator.exe"
+  "RAMSimulator.exe.manifest"
+  "RAMSimulator.pdb"
+  "libRAMSimulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RAMSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
