647015c81feb3d2814a959eb7d8d4dfe0cb1e2c8
Róger Roca Arano <rogerroca@reinseg.com>
2022-12-15 11:38:09 -0400

    Se crea la clase matrizDispersaVector

---
+++ lib/matrizDispersaVector.h
@@ -0,0 +1,43 @@
+//
+// Created by rogerroca on 14/12/2022.
+//
+
+#ifndef INF220_MATRIZDISPERSAVECTOR_H
+#define INF220_MATRIZDISPERSAVECTOR_H
+
+const int MAX_VECTOR_SIZE = 100;
+
+class matrizDispersaVector {
+    typedef int DATA_TYPE;
+private:
+    int Vf[MAX_VECTOR_SIZE]; // Filas
+    int Vc[MAX_VECTOR_SIZE]; // Columnas
+    DATA_TYPE Vd[MAX_VECTOR_SIZE];  // elementos
+    int df; // Dimensión filas
+    int dc; // Dimensión columnas
+    int nt; // Términos
+    DATA_TYPE repe; // es el elemento que se repetirá en la matriz
+    int obtenerPosicion(int f, int c);
+
+public:
+    matrizDispersaVector();
+
+    void dimensionar(int m, int n);
+
+    int dimension_fila();
+
+    int dimension_columna();
+
+    void poner(int f, int c, DATA_TYPE valor);
+
+    DATA_TYPE elemento(int f, int c);
+
+    void definir_valor_repetido(DATA_TYPE valor);
+
+    void eliminar(int pos);
+
+    void mostrar();
+};
+
+
+#endif //INF220_MATRIZDISPERSAVECTOR_H
